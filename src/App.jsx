c04a906da616f5c553daeb3e89e9b2091a099941
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-07-07 15:03:37 -0400

    routing set up in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Booking from "./pages/Booking";
+import Rooms from "./pages/Rooms";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Layout from "./ui/Layout";
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<dashboard />} />
-          <Route path="account" element={<account />} />
-          <Route path="booking" element={<booking />} />
-          <Route path="login" element={<login />} />
-          <Route path="rooms" element={<rooms />} />
-          <Route path="settings" element={<settings />} />
-          <Route path="users" element={<users />} />
-          <Route path="*" element={<pageNotFound />} />
+          <Route element={<Layout />}>
+            <Route index element={<Navigate replace to="Dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="account" element={<Account />} />
+            <Route path="booking" element={<Booking />} />
+            <Route path="rooms" element={<Rooms />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/account.jsx
+++
@@ -1,5 +0,0 @@
-const account () => {
-    return <h1>Jake Norman</h1>;
-}
-
-export default account;
