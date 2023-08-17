7a3eb20c11048c725c2ffce7b7ccf96d38b2f7ba
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-08-16 21:11:19 -0400

    individual booking page

--- src/features/bookings/BookingDetails.jsx
+++ src/features/bookings/BookingDetails.jsx
@@ -5,6 +5,7 @@ import { useCheckOut } from "../../features/check_in_out/useCheckOut";
 import { useBooking } from "./useBooking";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../../ui/Spinner";
+import { format, isToday } from "date-fns";
 
 function BookingDetails() {
   const { booking, isLoading } = useBooking();
@@ -16,7 +17,22 @@ function BookingDetails() {
 
   if (isLoading) return <Spinner />;
 
-  const { status, id: bookingId } = booking;
+  const {
+    status,
+    id: bookingId,
+    created_at,
+    startDate,
+    endDate,
+    numberNights,
+    numberGuests,
+    roomPrice,
+    extrasPrice,
+    totalPrice,
+    hasBreakfast,
+    isPaid,
+    rooms: { name: roomName },
+    guests: { fullName: guestName, email: email },
+  } = booking;
 
   return (
     <>
@@ -38,6 +54,25 @@ function BookingDetails() {
           </span>
         )}
         <button onClick={moveBack}>Back</button>
+      </div>
+      <div className="flex">
+        {numberNights} nights in room {roomName}
+        {format(new Date(startDate), "MMM dd yyyy")} &mdash;{" "}
+        {format(new Date(endDate), "MMM dd yyyy")}
+      </div>
+      <div>
+      {guestName} + {numberGuests - 1} guests
+      email: {email}
+      </div>
+      <div>
+      Breakfast included? {hasBreakfast ? 'yes' : 'no'}
+      </div>
+      <div>
+        Total price: ${(totalPrice * numberNights)}
+        {isPaid ? "Already paid" : "Will pay at check-in"}
+      </div>
+      <div>
+        Booked on {format(new Date(created_at), "EEE, MMM dd yyyy, p")}
       </div>
     </>
   );
